bda4ce26ce9190bc00879383f2137b329f306f8a
CitaVital <uip.bd2.citavital@gmail.com>
2023-12-13 20:01:30 -0500

    Se agrego favicon.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="icon" type="image/x-icon" href="https://citavitalassets.s3.amazonaws.com/images/icono.ico">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
